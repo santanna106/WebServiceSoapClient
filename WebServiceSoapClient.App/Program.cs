028d95534584fc38c8af0cb8a775b343e6073644
Gabriel Santanna <gabriel.santanna@nic.intranet>
2022-01-10 14:02:32 -0300

    First Commit

---
+++ WebServiceSoapClient.App/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using WebServiceSoapClient.App.ServiceContact;
+namespace WebServiceSoapClient.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WebServiceContact s = new WebServiceContact();
+            var lista = s.GetAllContacts();
+            Contact c = new Contact();
+            c.Nome = "Gabriel";
+            c.Email = "gabriel.teste@gmail.com";
+
+            s.AddContact(c);
+
+            var lista2 = s.GetAllContacts();
+
+        }
+    }
+}
